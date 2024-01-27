9386df382c6f790397df376b70c8450482d9db7d
rodrigo-a-mota <rodrigo.ram.mota@gmail.com>
2024-01-27 15:47:37 -0300

    romeve access keys

--- aula16-twiter.py
+++ aula16-twiter.py
@@ -3,11 +3,11 @@ import json
 import pprint
 import urllib
 
-consumer_key        = 'Z3k8WFxb2jsypIVGlrM5WFKS2'
-consumer_secret     = 'tC3lviFlYMWYk9u6kRiatOWBQUN63KWqabMUwShiRORHwhGbCg'
+consumer_key        = ''
+consumer_secret     = ''
 
-token_key           = '1746922138743771136-v2ce5iTKFTGLS7O2o4diKNUa0LQ17n'
-token_secret        = 'Q4S3JSW2IS5MTBdEwpCBUiWLTOtL44Zx853Sb0PJAV9gv'
+token_key           = ''
+token_secret        = ''
 
 consumer            = oauth2.Consumer(consumer_key, consumer_secret)
 token               = oauth2.Token(token_key, token_secret)
--- aula17-teste-bbiblioteca.py
+++ aula17-teste-bbiblioteca.py
@@ -1,10 +1,10 @@
 from twitter import Twitter
 
-consumer_key        = 'Z3k8WFxb2jsypIVGlrM5WFKS2'
-consumer_secret     = 'tC3lviFlYMWYk9u6kRiatOWBQUN63KWqabMUwShiRORHwhGbCg'
+consumer_key        = ''
+consumer_secret     = ''
 
-token_key           = '1746922138743771136-v2ce5iTKFTGLS7O2o4diKNUa0LQ17n'
-token_secret        = 'Q4S3JSW2IS5MTBdEwpCBUiWLTOtL44Zx853Sb0PJAV9gv'
+token_key           = ''
+token_secret        = ''
 
 twitter             = Twitter(consumer_key, consumer_secret, token_key, token_secret)
 response            = twitter.tweet('vamos testar a lib')
