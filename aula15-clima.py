5e55e0bfbca85992edcbd138b8ac9f4a6c9df509
rodrigo-a-mota <rodrigo.ram.mota@gmail.com>
2024-01-27 15:48:44 -0300

    romeve token

--- aula15-clima.py
+++ aula15-clima.py
@@ -3,7 +3,7 @@ import json
 
 city = input('Escreva sua cidade:')
 
-request = (requests.get('https://pro.openweathermap.org/data/2.5/forecast/climate?q='+city+'&appid=8cc0dba93e3cf352babfb5b24e332658'))
+request = (requests.get('https://pro.openweathermap.org/data/2.5/forecast/climate?q='+city+'&appid='))
 data    = json.loads(request.text)
 
 print('Condição do tempo: ', data)
